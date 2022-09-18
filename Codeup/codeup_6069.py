abef72d536ceb8831b955d1c1d77d53f3d6cdbd4
wnsghd14 <wnsghd14@gmail.com>
2022-09-19 07:54:22 +0900

    0919

---
+++ Codeup/codeup_6069.py
@@ -0,0 +1,11 @@
+n = input()
+if n == 'A':
+  print('best!!!')
+elif n == 'B':
+  print('good!!')
+elif n == 'C':
+  print('run!')
+elif n == 'D':
+  print('slowly~')
+else:
+  print('what?')
---
+++ Codeup/codeup_6070.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n == 12 or n == 1 or n == 2:
+  print('winter')
+elif n == 3 or n == 4 or n == 5:
+  print('spring')
+elif n == 6 or n == 7 or n == 8:
+  print('summer')
+elif n == 9 or n == 10 or n == 11:
+  print('fall')
