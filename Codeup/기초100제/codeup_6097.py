a6b5b3aff5e2da652811b6037645d908a52e2502
wnsghd14 <wnsghd14@gmail.com>
2022-09-28 00:11:40 +0900

    codeup done

--- Codeup/기초100제/codeup_6097.py
+++ Codeup/기초100제/codeup_6097.py
@@ -1,24 +1,18 @@
-h,w = input().split()
-h = int(h)
-w = int(w)
+h,w = map(int,input().split())
 
-m = []
-for i in range(h+1) :
-  m.append([])
-  for j in range(w+1) :
-    m[i].append(0)
+arr = [[0]*(w+1) for _ in range(h+1)]
 
 n = int(input())
 for i in range(n) :
-  l,d,x,y = input().split()
-  if int(d)==0 :
-    for j in range(int(l)) :
-      m[int(x)][int(y)+j] = 1
-  else :
-    for j in range(int(l)) :
-      m[int(x)+j][int(y)] = 1
+  l,d,x,y = map(int,input().split())
+  
+  for j in range(l) :
+    if d==0 :
+      arr[x][y+j] = 1
+    else :
+      arr[x+j][y] = 1
 
 for i in range(1, h+1) :
   for j in range(1, w+1) :
-    print(m[i][j], end=' ')
+    print(arr[i][j], end=' ')
   print()
--- Codeup/기초100제/codeup_6098.py
+++ Codeup/기초100제/codeup_6098.py
@@ -1,33 +1,24 @@
-m=[]
-for i in range(12) :
-  m.append([])
-  for j in range(12) :
-    m[i].append(0)
+arr = [list(map(int,input().split())) for _ in range(10)]
 
-for i in range(10) :
-  a=input().split()
-  for j in range(10) :
-    m[i+1][j+1]=int(a[j])
 
-x = 2
-y = 2
-while True :
-  if m[x][y] == 0 :
-    m[x][y] = 9
-  elif m[x][y] == 2 :
-    m[x][y] = 9
-    break
+x, y = 1, 1
 
-  if (m[x][y+1]==1 and m[x+1][y]==1) or (x==9 and y==9) :
-    break
+while True:
+    if (arr[x][y] == 0):
+        arr[x][y] = 9
+    elif (arr[x][y] == 2):
+        arr[x][y] = 9
+        break
 
-  if m[x][y+1] != 1 :
-    y += 1
-  elif m[x+1][y] != 1 :
-    x += 1
-    
+    if ((arr[x][y+1] == 1 and arr[x+1][y] == 1)):
+        break
 
-for i in range(1, 11) :
-  for j in range(1, 11) :
-    print(m[i][j], end=' ')
-  print()
+    if (arr[x][y+1] != 1):
+        y = y + 1
+    elif (arr[x+1][y] != 1):
+        x = x + 1
+
+for i in range(10):
+    for j in range(10):
+        print(arr[i][j], end=' ')
+    print()
