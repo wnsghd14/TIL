accbf154c7eb3587bd54a42995b6b2086dece051
wnsghd14 <wnsghd14@gmail.com>
2022-09-28 00:12:56 +0900

    re

--- Codeup/기초100제/codeup_6098.py
+++ Codeup/기초100제/codeup_6098.py
@@ -4,21 +4,21 @@ arr = [list(map(int,input().split())) for _ in range(10)]
 x, y = 1, 1
 
 while True:
-    if (arr[x][y] == 0):
-        arr[x][y] = 9
-    elif (arr[x][y] == 2):
-        arr[x][y] = 9
-        break
+  if (arr[x][y] == 0):
+    arr[x][y] = 9
+  elif (arr[x][y] == 2):
+    arr[x][y] = 9
+    break
 
-    if ((arr[x][y+1] == 1 and arr[x+1][y] == 1)):
-        break
+  if ((arr[x][y+1] == 1 and arr[x+1][y] == 1)):
+    break
 
-    if (arr[x][y+1] != 1):
-        y = y + 1
-    elif (arr[x+1][y] != 1):
-        x = x + 1
+  if (arr[x][y+1] != 1):
+    y = y + 1
+  elif (arr[x+1][y] != 1):
+    x = x + 1
 
 for i in range(10):
-    for j in range(10):
-        print(arr[i][j], end=' ')
-    print()
+  for j in range(10):
+    print(arr[i][j], end=' ')
+  print()
