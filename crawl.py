d0f86677bee9ab57437b30d48e245bacfdb33b01
wnsghd14 <wnsghd14@gmail.com>
2022-12-02 20:07:22 +0900

    crwaling practice

---
+++ crawl.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Anchor extraction from HTML document
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+with urlopen('https://en.wikipedia.org/wiki/Main_Page') as response:
+    soup = BeautifulSoup(response, 'html.parser')
+    for anchor in soup.find_all('a'):
+        print(anchor.get('href', '/'))
