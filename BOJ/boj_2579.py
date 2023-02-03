3e4a1e96f73869ef85c9f987e2e031cc0274142f
wnsghd14 <wnsghd14@gmail.com>
2023-02-03 21:47:03 +0900

    0203

--- BOJ/boj_2579.py
+++ BOJ/boj_2579.py
@@ -7,11 +7,14 @@ for k in range(n):
     m[k] = int(sys.stdin.readline())
 
 dp = [0] * 301
+
 dp[0] = m[0] 
 dp[1] = m[0] + m[1] 
+
 dp[2] = max(m[1] + m[2], m[0] + m[2]) 
 
 for i in range(3, n + 1):
+    
     dp[i] = max(dp[i - 3] + m[i - 1] + m[i], dp[i - 2] + m[i])
 
 print(dp[n - 1])
