bd35a49629af54d09dbe02918da92baf5cd35575
wnsghd14 <wnsghd14@gmail.com>
2023-02-12 23:26:38 +0900

    0212

---
+++ BOJ/boj_1463.py
@@ -0,0 +1,2 @@
+t = list(input())
+print(t)
---
+++ BOJ/boj_6198.py
@@ -0,0 +1,43 @@
+n = int(input())
+
+data = list(int(input()) for _ in range(n))
+
+cnt = 0
+stack = []
+
+for i in data:
+    while stack and stack[-1] <= i:
+        stack.pop()
+    cnt += len(stack)
+    stack.append(i)
+
+print(cnt)
+
+# import sys
+
+# input = sys.stdin.readline
+
+# N = int(input())
+# stack = []      # 옥상 스택
+# cnt = 0      # 결과값
+
+
+# h = int(input())
+# stack.append(h)
+# top = 0
+
+# for _ in range(N-1):
+#     h = int(input())
+
+#     while top > -1:
+#         if stack[top] > h:
+#             cnt += top + 1
+#             break
+
+#         stack.pop()
+#         top -= 1
+
+#     stack.append(h)
+#     top += 1
+
+# print(cnt)
