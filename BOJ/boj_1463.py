6b231e764f621e3a62683ca8d5ca97862bece6a5
wnsghd14 <wnsghd14@gmail.com>
2023-02-13 22:47:21 +0900

    0213-2

--- BOJ/boj_1463.py
+++ BOJ/boj_1463.py
@@ -1,9 +1,22 @@
+# import sys
+# n = int(sys.stdin.readline())
+
+# cnt = [0,0]
+
+# for i in range(2,n+1): 
+#     cnt.append(min(cnt[i//2]+i%2, cnt[i//3] + i%3) + 1)
+# print(cnt)
+# print(cnt[-1])
 import sys
-n = int(sys.stdin.readline())
+input = sys.stdin.readline
+n = int(input())
 
-cnt = [0,0]
+dp = [0]*(n+1)
 
-for i in range(2,n+1): 
-    cnt.append(min(cnt[i//2]+i%2, cnt[i//3] + i%3) + 1)
-print(cnt)
-print(cnt[-1])
+for i in range(2,n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+print(dp[n])
