d9aeeb8784844b7614bb79c97e94ce82a8bf0146
wnsghd14 <wnsghd14@gmail.com>
2023-02-15 23:37:37 +0900

    0215

--- BOJ/boj_13305.py
+++ BOJ/boj_13305.py
@@ -5,8 +5,10 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 oil = list(map(int, input().split()))
+
 mi = oil[0]
 result = 0
+
 for i in range(n-1):
     if mi > oil[i]:
         mi = oil[i]
