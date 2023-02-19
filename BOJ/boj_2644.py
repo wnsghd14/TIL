45e89f0dde1fe8f73c6d8df4370ed969ec31894c
wnsghd14 <wnsghd14@gmail.com>
2023-02-19 16:13:02 +0900

    0219

--- BOJ/boj_2644.py
+++ BOJ/boj_2644.py
@@ -1,30 +1,27 @@
 import sys
 sys.setrecursionlimit(300000)
 
-# def dfs(node):
-#     for n in graph[node]:
-#         if check[n] == 0:
-#             check[n] = check[node]+1
-#             dfs(n)
+def dfs(node):
+    for n in graph[node]:
+        if check[n] == 0:
+            check[n] = check[node]+1
+            dfs(n)
             
-# n = int(input())
-# graph = [[] for _ in range(n+1)]
-# s, e = map(int, input().split())
-# for _ in range(int(input())):
-#     u, v = map(int, input().split())
-#     graph[u].append(v)
-#     graph[v].append(u)
-# check = [0]*(n+1)
-# dfs(s)
-# print(check[e] if check[e] > 0 else -1)
+n = int(input())
+graph = [[] for _ in range(n+1)]
+s, e = map(int, input().split())
+for _ in range(int(input())):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+check = [0]*(n+1)
+dfs(s)
+print(check[e] if check[e] > 0 else -1)
 
-print('=================')
-def pprint(arr):
-    for i in range(len(arr)):
-        print(i, arr[i])
 
 
 n = int(input())
+
 start,end = list(map(int,input().split()))
 
 m = int(input())
