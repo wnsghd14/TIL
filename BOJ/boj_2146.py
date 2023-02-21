2630adc7a339ed619a59559f4ba6dcdb0e6d72fc
wnsghd14 <wnsghd14@gmail.com>
2023-02-21 20:27:07 +0900

    0221

--- BOJ/boj_2146.py
+++ BOJ/boj_2146.py
@@ -1,27 +1,73 @@
+import sys
 from pprint import pprint
 from collections import deque
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    visited[x][y] = True
-    while q:
-        x, y = q.popleft()
-        for k in range(4):
-            nx = x + dx[k] 
-            ny = y + dy[k]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if arr[nx][ny] == 1 and visited[nx][ny] == False:
-                q.append((nx,ny))
-                visited[nx][ny] = True
+input = sys.stdin.readline
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs(a, b):
+    global land
+    maps[a][b] = land
+    visit[a][b] = 1
+
+    queue = deque()
+    queue.append((a, b))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and not visit[nx][ny] and maps[nx][ny]:
+                queue.append((nx, ny))
+                visit[nx][ny] = 1
+                maps[nx][ny] = land
+
+def check(k):
+    global ans
+    dis = [[-1] * n for _ in range(n)]
+    queue = deque()
+
+
+    for i in range(n):
+        for j in range(n):
+            if maps[i][j] == k:
+                queue.append((i, j))
+                dis[i][j] = 0
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if maps[nx][ny] > 0 and maps[nx][ny] != k:
+
+                    ans = min(ans, dis[x][y])
+                elif not maps[nx][ny] and dis[nx][ny] == -1:
+                    dis[nx][ny] = dis[x][y] + 1
+                    queue.append((nx, ny))
+    
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False]*n for _ in range(n)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+
+maps = [list(map(int, input().split())) for _ in range(n)]
+visit = [[0] * n for _ in range(n)]
+
+land = 1
 
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 1 and visited[i][j] == False:
+        if not visit[i][j] and maps[i][j]:
             bfs(i, j)
+            land += 1
+
+ans = sys.maxsize
+
+for i in range(1, land):
+    check(i)
+print(ans)
